97e041e2fb9b11e235cd987b05ae021a71b81264
GastonDiazA <gaston.diazazar94@gmail.com>
2021-11-03 17:37:54 -0300

    Gas
    Acomodado de prefabs

--- Assets/Scripts/AllPuzzlesResolve.cs
+++ Assets/Scripts/AllPuzzlesResolve.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class AllPuzzlesResolve : MonoBehaviour
 {
     public static int allPuzzlesResolved;
+    public Transform platformBridge1;
+    public Transform platformBridge2;
+    public Transform platformBridge3;
+    
     
     public GameObject winExitMenu;
     
@@ -20,13 +25,14 @@ public class AllPuzzlesResolve : MonoBehaviour
     void Update()
     {
         if (allPuzzlesResolved == 3){
-            winExitMenu.SetActive(true);
-            Time.timeScale = 0f;
-            //allPuzzlesResolved = 0;
+            //winExitMenu.SetActive(true);
+            //Time.timeScale = 0f;
+            BridgeMovement();
+            
         }
         else
         {
-            winExitMenu.SetActive(false);
+            //winExitMenu.SetActive(false);
         }
         
     }
@@ -34,5 +40,13 @@ public class AllPuzzlesResolve : MonoBehaviour
     public void countPuzzlesSolved()
     {
         allPuzzlesResolved++;
+    }
+
+    private void BridgeMovement()
+    {
+        var platform1V3 = platformBridge1.position; 
+        Vector3 movement1 = new Vector3(platform1V3.x, platform1V3.y,
+            platform1V3.z + 1.8f);
+        Vector3.Lerp(platform1V3, movement1, 1f);
     }
 }
--- Assets/Scripts/Rotations/NewRotations.cs
+++ Assets/Scripts/Rotations/NewRotations.cs
@@ -2,21 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class NewRotations : MonoBehaviour
 {
+    public Transform starPosition;
+    public Transform endPosition;
     private Quaternion _currentRotation;
     public float degrees;
+    public float lerpSlider = 1f;
+    public float distanceToSleerp;
 
     void Update()
     {
+       BridgeMovement();
         _currentRotation = transform.rotation;
         if (Input.GetKeyDown(KeyCode.E))
         {
-            var angle = transform.localEulerAngles.x;
-            {
-                
-                transform.rotation = Quaternion.Euler(_currentRotation.x++, _currentRotation.y, _currentRotation.x);
-            }
+            BridgeMovement();
+            // var angle = transform.localEulerAngles.x;
+            // {
+            //     
+            //     transform.rotation = Quaternion.Euler(_currentRotation.x++, _currentRotation.y, _currentRotation.x);
+            // }
             
         }
         if (Input.GetKeyDown(KeyCode.Q))
@@ -28,5 +35,12 @@ public class NewRotations : MonoBehaviour
             
         }
         
+    }
+    private void BridgeMovement()
+    {
+        
+        Vector3 movement1 = new Vector3(transform.position.x, transform.position.y,
+            transform.position.z + distanceToSleerp);
+        transform.position = Vector3.Lerp(starPosition.position, endPosition.position,  lerpSlider * Time.deltaTime * 0.01f);
     }
 }
